b634066404dc52702edcae9c23805c65c7d2d0db
Judmartinez07 <jmartinez20670@iesjoanramis.org>
2024-11-06 11:59:25 +0100

    Add files via upload

---
+++ Organiza tu armario/js/script.js
@@ -0,0 +1,6 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js" 
+
+
+const input = document.getElementById("añadir")
+const boto = document.getElementById("afegir")
