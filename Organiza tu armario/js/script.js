f6b34871f9184a5a5daa64d74d672f18679adf1d
Judmartinez07 <jmartinez20670@iesjoanramis.org>
2024-11-12 12:54:06 +0100

    Add files via upload

--- Organiza tu armario/js/script.js
+++ Organiza tu armario/js/script.js
@@ -2,7 +2,44 @@ import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-
 import {getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js" 
 
 
-const input = document.getElementById("añadir")
-const boto = document.getElementById("afegir")
+const appSettings = {
+    databaseURL: "https://organiza-tu-ropa-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+const app = initializeApp(appSettings);
+const baseDades = getDatabase(app);
+const l1 = ref(baseDades, "Look 1 ");
+
+
+
+onValue(l1, function(snapshot) {
+    console.log(snapshot)
+    if (snapshot.exists()){
+
+        let resultats = Object.entries(snapshot.val())
+
+        for (let i = 0; i < resultats.length; i++) {
+            let current = resultats[i]
+            console.log(current)
+            addElement(current)
+        } 
+  
+    }
+
+   
+
+})
+
+function addElement(e){
+    let titol = document.createElement("h1");
+    titol.textContent=e[0]
+    let show = document.createElement("img");
+    show.src = e[1]
+
+    let c = document.getElementById("l1");
+    c.appendChild(titol);
+    c.appendChild(show);
+
+
+}
 
 
